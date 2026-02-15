01b40d5ff959a28870cf4e44d10b2ed691bdb522
Subhajyotioo7 <programingsubhajyoti@gmail.com>
2026-02-16 00:12:18 +0530

    Initial React Vite project

---
+++ react-vite-demo/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>🚀 React + Vite</h1>
+      <p>Auto deploy ready project</p>
+    </div>
+  );
+}
