59781f9d9a337b33fe6c9dc96c6b19717d5502bf
Subhajyotioo7 <programingsubhajyoti@gmail.com>
2026-02-16 01:35:04 +0530

    chang

--- react-vite-demo/src/App.jsx
+++ react-vite-demo/src/App.jsx
@@ -2,7 +2,8 @@ export default function App() {
   return (
     <div style={{ padding: "40px", textAlign: "center" }}>
       <h1>🚀 React + Vite</h1>
-      <p>Auto deploy ready project</p>
+      <p>Auto deploy ready   project</p>
+      <p>Auto deploy readyyyy   project</p>
     </div>
   );
 }
